df4b33ba95cc06ab5236396cc22ee300781be4f8
pies-are-squares <152812494+pies-are-squares@users.noreply.github.com>
2024-07-17 09:25:59 -0400

    Layers!

--- js/layers.js
+++ js/layers.js
@@ -1,6 +1,6 @@
 addLayer("p", {
-    name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
+    name: "Fir", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "F", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
@@ -8,7 +8,7 @@ addLayer("p", {
     }},
     color: "#4BDC13",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "prestige points", // Name of prestige currency
+    resource: "deciduous points", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
@@ -18,11 +18,29 @@ addLayer("p", {
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
+	    if (hasUpgrade('p', 11)) gain = gain.plus(1)
+	    if (hasUpgrade('p',12)) gain = gain.times(upgradeEffect('p',12))
         return new Decimal(1)
     },
+ effect() {
+        return player[this.layer].points.add(1).pow(0.5)
+    },
+    effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "d", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return true}
+    upgrades: {
+ 11: {
+title: "Getting Started",
+    description: "Gain an extra point per second.",
+    cost: new Decimal(1),
+        },
+12: {
+title: "Silver"
+	description: "Gain more points based on your fir points." 
+	cost: new Decimal(2),
+	
+    },
 })
